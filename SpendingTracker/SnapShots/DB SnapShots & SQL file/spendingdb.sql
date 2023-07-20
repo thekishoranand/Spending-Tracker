9e57e1663683ee89f0dfcca3f011df8a5dfe211e
thekishoranand <91248337+thekishoranand@users.noreply.github.com>
2023-07-21 01:47:13 +0530

    Add files via upload

---
+++ SpendingTracker/SnapShots/DB SnapShots & SQL file/spendingdb.sql
@@ -0,0 +1,76 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: spendingdb
+-- ------------------------------------------------------
+-- Server version	8.0.4-rc-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category_info`
+--
+
+DROP TABLE IF EXISTS `category_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category_info` (
+  `category` varchar(50) NOT NULL,
+  PRIMARY KEY (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category_info`
+--
+
+LOCK TABLES `category_info` WRITE;
+/*!40000 ALTER TABLE `category_info` DISABLE KEYS */;
+INSERT INTO `category_info` VALUES ('Coffee'),('CookingOil'),('Egg'),('Sugar'),('Tomato');
+/*!40000 ALTER TABLE `category_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `spendings`
+--
+
+DROP TABLE IF EXISTS `spendings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `spendings` (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(50) NOT NULL,
+  `sdate` date NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `spendings`
+--
+
+LOCK TABLES `spendings` WRITE;
+/*!40000 ALTER TABLE `spendings` DISABLE KEYS */;
+INSERT INTO `spendings` VALUES (1,'Sugar','2020-04-29',200),(3,'CookingOil','2020-04-28',350),(4,'Coffee','2019-04-13',666),(5,'Coffee','2020-04-08',99),(6,'Coffee','2020-04-09',88),(10,'CookingOil','2020-04-17',456),(11,'Sugar','2020-04-16',785),(13,'Tomato','2020-04-28',100);
+/*!40000 ALTER TABLE `spendings` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-29 10:51:23
